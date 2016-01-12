317e882e3814ce58eceef425f9da298f92284560
ravier <clementra.ravier@gmail.com>
2016-01-12 15:48:59 +0100

    Ajustement de la fenêtre graphique principale part rapport au jets papiers initiaux

--- src/geoimbib/Views/JPanels/V_JPanelMainLeft.java
+++ src/geoimbib/Views/JPanels/V_JPanelMainLeft.java
@@ -4,6 +4,7 @@ import geoimbib.Views.V_MainWindow;
 
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
 import java.awt.*;
 
 /**
@@ -14,7 +15,12 @@ public class V_JPanelMainLeft extends JPanel{
     private V_MainWindow v_mainWindow;
 
     //JList
-    JList jList = null;
+    private JList jList = null;
+    private JList jListEchantillons = null;
+
+    //JPanel changement de répertoire liste de tests
+    private JTextField jtextfieldFolder = null;
+    private JPanel jpanelFolder = null;
 
     public V_JPanelMainLeft(V_MainWindow v_mainWindow){
         this.v_mainWindow = v_mainWindow;
@@ -29,25 +35,75 @@ public class V_JPanelMainLeft extends JPanel{
 
         this.setBorder(new BevelBorder(BevelBorder.RAISED));
 
-        //tableau de Strings pour exemple jlist
-        String [] tabStringExemple = new String[50];
-        for (int i = 0; i<tabStringExemple.length; ++i){
-            tabStringExemple[i] = "String "+i;
-        }
+        /*
+        * JTextfield changement de répertoire
+        * */
+            jpanelFolder = new JPanel();
+            jpanelFolder.setLayout(new BoxLayout(jpanelFolder, BoxLayout.X_AXIS));
+            jtextfieldFolder = new JTextField(); //rechercher la préférence lors de l'initialisation de la vue
 
-        jList = new JList(tabStringExemple); //data has type Object[]
-        jList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
-        //jList.setLayoutOrientation(JList.HORIZONTAL_WRAP);
-        jList.setVisibleRowCount(-1);
-        JScrollPane listScroller = new JScrollPane(jList);
-        listScroller.setPreferredSize(new Dimension(150, 250));
+            JButton jButtonPathFolder = new JButton("...");
 
-        this.add(listScroller);
 
-        listScroller.setBounds(getWidth()/2 - (int)listScroller.getPreferredSize().getWidth()/2,
-                getHeight()/2 - (int)listScroller.getPreferredSize().getHeight()/2,
-                150,
-                250);
+            this.add(jpanelFolder);
+            jpanelFolder.add(jtextfieldFolder);
+            jpanelFolder.add(jButtonPathFolder);
+            jpanelFolder.setBounds(
+                    getWidth()/2 - (int)jpanelFolder.getPreferredSize().getWidth(),
+                    getHeight()/4,
+                    250,
+                    25
+            );
+
+
+        /*
+        * JList 1 qui répertorie toute les expériences d'un répertoire
+        * */
+
+            //tableau de Strings pour exemple jlist
+            String [] tabStringExemple = new String[50];
+            for (int i = 0; i<tabStringExemple.length; ++i){
+                tabStringExemple[i] = "Série "+i;
+            }
+
+            jList = new JList(tabStringExemple); //data has type Object[]
+            jList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+            //jList.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+            jList.setVisibleRowCount(-1);
+            JScrollPane listScroller = new JScrollPane(jList);
+            listScroller.setPreferredSize(new Dimension(150, 250));
+
+            this.add(listScroller);
+
+            listScroller.setBounds(getWidth()/4 - (int)listScroller.getPreferredSize().getWidth()/2,
+                    getHeight()/2 - (int)listScroller.getPreferredSize().getHeight()/2,
+                    150,
+                    250);
+
+        /*
+        * JList 2 qui affiche tous les échantillons d'une expérience
+        * */
+
+            //tableau de Strings pour exemple jlist
+            String [] tabStringExemple2 = new String[20];
+            for (int i = 0; i<tabStringExemple2.length; ++i){
+                tabStringExemple2[i] = "Echantillon "+i;
+            }
+
+            jListEchantillons = new JList(tabStringExemple2); //data has type Object[]
+            jListEchantillons.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+            //jList.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+            jListEchantillons.setVisibleRowCount(-1);
+            JScrollPane listScroller2 = new JScrollPane(jListEchantillons);
+            listScroller2.setPreferredSize(new Dimension(150, 250));
+
+            this.add(listScroller2);
+
+            listScroller2.setBounds(getWidth()/2 + (int)listScroller2.getPreferredSize().getWidth()/2,
+                    getHeight()/2 - (int)listScroller2.getPreferredSize().getHeight()/2,
+                    150,
+                    250);
     }
+
 
 }
--- src/geoimbib/Views/JPanels/V_JPanelMainRight.java
+++ src/geoimbib/Views/JPanels/V_JPanelMainRight.java
@@ -15,13 +15,18 @@ public class V_JPanelMainRight extends JPanel {
     private V_MainWindow v_mainWindow;
 
     //JPanel des boutons
-    JPanel jpanelButtons = null;
+    private JPanel jpanelButtons = null;
+    private JPanel jpanelLeave = null;
 
     //Boutons
     JButton jbutton1 = null;
     JButton jbutton2 = null;
     JButton jbutton3 = null;
     JButton jbutton4 = null;
+    JButton jbutton5 = null;
+
+    JButton jbuttonLeave = null;
+
 
     public V_JPanelMainRight(V_MainWindow v_mainWindow){
         this.v_mainWindow = v_mainWindow;
@@ -36,23 +41,37 @@ public class V_JPanelMainRight extends JPanel {
 
         this.setBorder(new BevelBorder(BevelBorder.RAISED));
 
-        jpanelButtons = new JPanel(new GridLayout(4,1));
+        jpanelButtons = new JPanel(new GridLayout(5,1));
         this.add(jpanelButtons);
 
-        jbutton1 = new JButton("JButton 1");
+        jbutton1 = new JButton("Nouvelle série");
         jpanelButtons.add(jbutton1);
-        jbutton2 = new JButton("JButton 2");
+        jbutton2 = new JButton("Fusion de séries");
         jpanelButtons.add(jbutton2);
-        jbutton3 = new JButton("JButton 3");
+        jbutton3 = new JButton("Retirer échantillon");
         jpanelButtons.add(jbutton3);
-        jbutton4 = new JButton("JButton 4");
+        jbutton4 = new JButton("Graphique");
         jpanelButtons.add(jbutton4);
+        jbutton5 = new JButton("Supprimer");
+        jpanelButtons.add(jbutton5);
 
 
-        jpanelButtons.setBounds(getWidth()/2 - (int)jpanelButtons.getPreferredSize().getWidth(),
+        jpanelButtons.setBounds(getWidth()/2 - (int)jpanelButtons.getPreferredSize().getWidth()/2,
                 getHeight()/2 - (int)jpanelButtons.getPreferredSize().getHeight(),
                 200,
-                200);
+                250);
+
+        jpanelLeave = new JPanel(new GridLayout(1,1));
+        jbuttonLeave = new JButton("Quitter");
+        jpanelLeave.add(jbuttonLeave);
+
+
+        jpanelLeave.setBounds(getWidth()/2 - (int)jpanelLeave.getPreferredSize().getWidth(),
+                getHeight() - (int)jpanelLeave.getPreferredSize().getHeight()*3,
+                200,
+                50);
+        this.add(jpanelLeave);
+
     }
 
 }
